70feb1f11dc4e1b63636c3b1f1392a8e1f975d3d
George Wu <micstu@qq.com>
2016-01-03 22:36:54 +0800

    Update DisplayObjectContainer.addChildAt(), added 2 special cases handling.

--- src/flash/display/DisplayObjectContainer.ts
+++ src/flash/display/DisplayObjectContainer.ts
@@ -36,7 +36,13 @@ export abstract class DisplayObjectContainer extends InteractiveObject {
 
     addChildAt(child:DisplayObject, index:number):DisplayObject {
         if (this._children.indexOf(child) < 0) {
-            this._children = this._children.slice(0, index - 1).concat(child).concat(this._children.slice(index, this._children.length - 1));
+            if (index === 0) {
+                this._children.unshift(child);
+            } else if (index === this._children.length - 1) {
+                this._children.push(child);
+            } else {
+                this._children = this._children.slice(0, index - 1).concat(child).concat(this._children.slice(index, this._children.length - 1));
+            }
         }
         child.childIndex = index;
         return child;
