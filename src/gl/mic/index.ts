f55886e843a764e68722e0d04b71752ca4494bff
George Wu <uiharu@buaa.edu.cn>
2016-08-22 14:11:08 +0200

    fix

--- src/gl/mic/index.ts
+++ src/gl/mic/index.ts
@@ -2,10 +2,12 @@
  * Created by MIC on 2016/8/22.
  */
 
+export * from "./BitConverter";
+export * from "./CommonUtil";
 export * from "./EngineBase";
 export * from "./ErrorBase";
+export * from "./EventBase";
 export * from "./MathUtil";
-export * from "./BitConverter";
 export * from "./VirtualDom";
 
 import * as glantern from "./glantern/index";
